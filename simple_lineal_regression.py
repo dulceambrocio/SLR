48f5f52856ba3ba77c04d572911b096f0510b6b5
dulceambrocio <dulce.ambrocio@hotmail.com>
2017-07-29 04:36:25 -0500

    all files added

---
+++ simple_lineal_regression.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+#############################
+# I 
+#############################
+np.random.seed(0)
+x=np.random.random(size=(100,))
+y=3.0*x+np.random.normal(0.0,0.1,size=(100,))
+
+xp=np.linspace(-0.5,1.5,201)
+import code; code.interact(local=locals())
+
+
+
+
+#############################
+# II
+#############################
